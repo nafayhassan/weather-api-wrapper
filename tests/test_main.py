398a7bd10469123600ccbd865ed865d1520ba7fc
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-06 00:14:29 +0100

    backend test code commit by ENGR. IPAYE

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,3 +10,14 @@ def test_ping():
     response = client.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"message": "pong"}
+
+
+# ----- WEATHER BY CITY ------- #
+def test_weather_by_city():
+    response = client.get("/weather/lagos")
+    assert response.status_code == 200
+    data = response.json()
+    assert "temperature" in data
+    assert "description" in data
+    assert "city" in data
+    assert data["city"].lower() == "lagos"
