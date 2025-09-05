d44630b46e5fc7a5df29f116e01c5da5485915a5
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-05 23:55:03 +0100

    backend test code commit by ENGR. IPAYE

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,3 +2,11 @@ import pytest
 from fastapi.testclient import TestClient
 from main import app
 
+client = TestClient(app)
+
+
+# -------- HEALTH CHECK ----------#
+def test_ping():
+    response = client.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == {"message": "pong"}
