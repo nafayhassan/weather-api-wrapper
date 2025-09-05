5b7f5cb7f279f00c2daeda6902eb3ef08434bb53
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-05 20:26:52 +0100

    backend code commit by ENGR. IPAYE

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from datetime import datetime
+from database import Base
+
+
+class WeatherHistory(Base):
+    __tablename__ = "weather_history"
+
+    id = Column(Integer, primary_key=True, index=True)
+    city = Column(String, index=True)
+    temperature = Column(Float)
+    description = Column(String)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+
