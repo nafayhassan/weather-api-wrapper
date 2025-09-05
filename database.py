bb9d2a9bc494c1d72c3ade58fa7d127a0eb14f38
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-05 20:17:29 +0100

    backend code commit by ENGR. IPAYE

--- database.py
+++ database.py
@@ -7,3 +7,4 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
+
